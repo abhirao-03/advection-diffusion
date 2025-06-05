04681d868e3cb95baf7edcec66443b062353c4e7
Abhijeet Rao <abhijeetraop@gmail.com>
2025-06-05 16:43:53 +0100

    compilation and advection diffusion variable storage adaptiaon.

--- Simulation.h
+++ Simulation.h
@@ -10,11 +10,22 @@ class Simulation{
         SimulationParams params;
         std::vector<GasCanister> canisters;
         std::vector<std::vector<double>> concentration_grid;
+        double diffusion_coeff_x;
+        double diffusion_coeff_y;
+        double advection_velocity_x;
+        double advection_velocity_y;
+        double time_step_dt;
 
     public:
-        explicit Simulation(const SimulationParams& initial_params):
+        explicit Simulation(const SimulationParams& initial_params,
+                            double pde_Dx, double pde_Dy,
+                            double pde_ux, double pde_uy,
+                            double pde_dt):
             params(initial_params),
-            concentration_grid(initial_params.get_y_steps(), std::vector<double>(initial_params.get_x_steps(), 0.0))
+            concentration_grid(initial_params.get_y_steps(), std::vector<double>(initial_params.get_x_steps(), 0.0)),
+            diffusion_coeff_x(pde_Dx), diffusion_coeff_y(pde_Dy),
+            advection_velocity_x(pde_ux), advection_velocity_y(pde_uy),
+            time_step_dt(pde_dt)
             {};
             
         void initialize_concentration_field();
--- main.cpp
+++ main.cpp
@@ -6,5 +6,50 @@
 #include "GasCanister.h"
 
 int main() {
+    int num_points_x = 10;
+    int num_points_y = 10;
+    double length_x = 1.0;
+    double length_y = 1.0;
 
+    double dc_x = 1.0;
+    double dc_y = 1.0;
+    double av_x = 1.0;
+    double av_y = 1.0;
+    double dt = 0.001;
+
+    SimulationParams params(length_x, length_y, num_points_x, num_points_y);
+
+    Simulation sim(params,
+                   dc_x,
+                   dc_y,
+                   av_x,
+                   av_y,
+                   dt);
+
+    GasCanister canister1(0.25, 0.25, 100.0, 0.1);
+    GasCanister canister2(0.5, 0.5, 10.0, 0.1);
+
+    sim.add_canister(canister1);
+    sim.add_canister(canister2);
+
+    sim.initialize_concentration_field();
+
+    const auto& grid = sim.get_concentration_grid();
+
+    // Print the grid
+    std::cout << "Concentration Grid (iy from 0 at top to Ny-1 at bottom):" << std::endl;
+    for (int iy = 0; iy < params.get_y_steps(); ++iy) {
+        for (int ix = 0; ix < params.get_x_steps(); ++ix) {
+            // std::fixed and std::setprecision ensure consistent decimal formatting
+            std::cout << std::fixed << std::setprecision(2) << grid[iy][ix] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    // For debugging, print dx and dy
+    std::cout << "dx = " << params.get_dx() << std::endl;
+    std::cout << "dy = " << params.get_dy() << std::endl;
+
+
+    return 0;
 }
